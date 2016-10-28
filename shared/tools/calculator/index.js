1afd903952cbf134c94684bbddc35b3efa5b94da
Romel Pérez <ronelprhone@gmail.com>
2016-10-27 21:53:14 -0500

    [test] shared

---
+++ shared/tools/calculator/index.js
@@ -0,0 +1,9 @@
+function sum (a = 0, b = 0) {
+  return a + b;
+}
+
+function multiply (a = 0, b = 0) {
+  return a * b;
+}
+
+export { sum, multiply };
---
+++ test/mocha-setup.js
@@ -0,0 +1,11 @@
+require('babel-core/register');
+require('babel-polyfill');
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+global.chai = chai;
+global.expect = chai.expect;
+global.assert = chai.assert;
+
+chai.use(chaiHttp);
