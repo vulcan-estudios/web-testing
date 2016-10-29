f76b6ca26b55363e511c838f62f4bd1433496377
Romel Pérez <ronelprhone@gmail.com>
2016-10-28 20:02:58 -0500

    [chore] vagrant

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -15,6 +15,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sleep 5s # Wait for mongod to be available
 sudo service mongod start
 
 ## Database
