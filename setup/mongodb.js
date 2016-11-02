d7d6d4dd56bae4f3818fe76bfda69d52b5b1a43c
Romel Pérez <ronelprhone@gmail.com>
2016-11-02 14:30:32 -0500

    [chore] setup

--- setup/mongodb.js
+++ setup/mongodb.js
@@ -27,22 +27,22 @@ db.createCollection('users');
 const usersList = [{
   	"_id" : ObjectId("5813bc184b57665d404678bc"),
   	"name" : "Pepe Pérez",
-  	"email" : "pepeperez@mail.com",
+  	"email" : "pepeperez@gmail.com",
   	"pwd" : "11111111"
   }, {
   	"_id" : ObjectId("5813bc184b57665d404678bd"),
   	"name" : "María Rodriguez",
-  	"email" : "mariarodriguez@mail.com",
+  	"email" : "mariarodriguez@gmail.com",
   	"pwd" : "22222222"
   }, {
   	"_id" : ObjectId("5813bc184b57665d404678be"),
   	"name" : "Carlos Martínez",
-  	"email" : "carlosmartinez@mail.com",
+  	"email" : "carlosmartinez@outlook.com",
   	"pwd" : "33333333"
   }, {
   	"_id" : ObjectId("5813bc184b57665d404678bf"),
   	"name" : "Sandra Molina",
-  	"email" : "sandramolina@mail.com",
+  	"email" : "sandramolina@yahoo.com",
   	"pwd" : "44444444"
 }];
 
