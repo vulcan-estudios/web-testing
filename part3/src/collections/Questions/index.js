7d2f89b4b89828347291e52c067727513aee5632
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 19:32:16 +0800

    Delete part3 directory

--- part3/src/collections/Questions/index.js
+++
@@ -1,9 +0,0 @@
-const _ = require('underscore');
-const Backbone = require('backbone');
-const Question = require('../../models/Question');
-
-const Collection = Backbone.Collection.extend({
-  model: Question
-});
-
-module.exports = Collection;
